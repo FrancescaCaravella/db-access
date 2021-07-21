99c0fc99e093d30cb4f2227621cfd660f64bbccb
franc <franc@LAPTOP-I8R5JNHP>
2021-07-21 23:13:28 +0200

    inizio

---
+++ src/dbaccess/prova/Prova.java
@@ -0,0 +1,15 @@
+package dbaccess.prova;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Prova {
+	
+	private static final Logger log = LogManager.getLogger(Prova.class);
+	
+	public static void main(String[] args) {
+		
+		log.info("CIAO RAGA");
+	}
+
+}
---
+++ src/dbaccess/tests/ProvaTest.java
@@ -0,0 +1,14 @@
+package dbaccess.tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ProvaTest {
+
+	@Test
+	void test() {
+		assertEquals(1,1);
+	}
+
+}
